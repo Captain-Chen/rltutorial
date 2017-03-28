3f7b7e947da096e7d15375516d28cc0be904f304
Richard Nguyen <almachy@gmail.com>
2017-03-28 07:47:40 -0400

    revised isPassable and drawTile function to use an array index. added interactDoor function

--- main.cpp
+++ main.cpp
@@ -2,30 +2,50 @@
 #include "Screen.h"
 
 // map variable
-const int map_sizeY = 11;
-const int map_sizeX = 20;
-int map[map_sizeY][map_sizeX] = {
+const int MAP_HEIGHT = 11;
+const int MAP_WIDTH = 20;
+int map[MAP_HEIGHT][MAP_WIDTH] = {
 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 { 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 { 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0 },
-{ 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0 },
+{ 0, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0 },
 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0 },
-{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0 },
+{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0 },
 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 };
 
-// initialize map info
-const int TILE_FLOOR = 0;
-const int TILE_WALL = 1;
-
 // map functions
 void initMap();
-void drawMap();
+void drawTile();
 bool isPassable(int mapX, int mapY);
+void interactDoor();
+
+
+// player variables
+int nPlayerX;
+int nPlayerY;
+
+struct TILE_TYPE{
+	char nCharacter; // ASCII character
+	short nColorCode; // Colour code
+	bool bPassable; // Can you walk on this tile?
+};
+
+TILE_TYPE sTileIndex[] = {
+	{'.', 7, true},   // 0 TILE_ROCKFLOOR
+	{'#', 7, false},  // 1 TILE_WALL
+	{'+', 6, false},  // 2 TILE_CLOSEDDOOR
+	{'/', 6, true}	  // 3 TILE_OPENDOOR
+};
+
+const int TILE_ROCKFLOOR = 0;
+const int TILE_WALL = 1;
+const int TILE_CLOSEDDOOR = 2;
+const int TILE_OPENDOOR = 3;
 
 int main(void)
 {
@@ -35,95 +55,100 @@ int main(void)
 	int ch = getch();
 
 	// initialize player character and position
-	int playerPosX = 10; 
-	int playerPosY = 10;
+	nPlayerX = 10; 
+	nPlayerY = 10;
+	int nDeltaX, nDeltaY;
 	char player = '@';
+
+	// colour definition
+	start_color();
+	init_pair(6, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
 
 	// main game loop
 	while(true){
 		// clear and add player character to screen; draw the map
 		// map is drawn first then the player character is drawn on top
 		clear();
-		drawMap();
-		mvaddch(playerPosY, playerPosX, player);
-
+		drawTile();
+		attron(COLOR_PAIR(6));
+		mvaddch(nPlayerY, nPlayerX, player);
+		// reset nDeltaX and nDeltaY
+		nDeltaX = nDeltaY = 0;
+		attroff(COLOR_PAIR(6));
 		// check for user input again
 		ch = getch();
 		switch(ch){
 			// upper-left
 			case 'y':
-				if(isPassable(playerPosX - 1, playerPosY - 1)){
-					playerPosX--;
-					playerPosY--;
-				}
+				nDeltaX = -1;
+				nDeltaY = -1;
 				break;
 			// upper-right
 			case 'u':
-				if(isPassable(playerPosX + 1, playerPosY - 1)){
-					playerPosX++;
-					playerPosY--;
-				}
+				nDeltaX = 1;
+				nDeltaY = -1;
 				break;
 			// left
 			case 'h':
-				if(isPassable(playerPosX - 1, playerPosY)){
-					playerPosX--;
-				}
+				nDeltaX = -1;
+				nDeltaY = 0;
 				break;
 			// down
 			case 'j':
-				if(isPassable(playerPosX, playerPosY + 1)){
-					playerPosY++;
-				}
+				nDeltaX = 0;
+				nDeltaY = 1;
 				break;
 			// up
 			case 'k':
-				if(isPassable(playerPosX, playerPosY - 1)){
-					playerPosY--;
-				}
+				nDeltaX = 0;
+				nDeltaY = -1;
 				break;
 			// right
 			case 'l':
-				if(isPassable(playerPosX + 1, playerPosY)){
-					playerPosX++;
-				}
+				nDeltaX = 1;
+				nDeltaY = 0;
 				break;
 			// bottom-left
 			case 'b':
-				if(isPassable(playerPosX - 1, playerPosY + 1)){
-					playerPosX--;
-					playerPosY++;
-				}
+				nDeltaX = -1;
+				nDeltaY = 1;
 				break;
 			// bottom-right
 			case 'n':
-				if(isPassable(playerPosX + 1, playerPosY + 1)){
-					playerPosX++;
-					playerPosY++;
-				}
+				nDeltaX = 1;
+				nDeltaY = 1;
+				break;
+			// open door
+			case 'o':
+				interactDoor();
+				break;
+			// close door
+			case 'c':
+				interactDoor();
 				break;
 			case 'q':
 				return 0;
 			default:
 				break;
 		}
+		if(isPassable(nPlayerX + nDeltaX, nPlayerY + nDeltaY)){
+			nPlayerX += nDeltaX;
+			nPlayerY += nDeltaY;
+		}
 	}
 	return 0;
 }
 
-// draw the map
-void drawMap(){
-	for(int mapy = 0; mapy < map_sizeY; mapy++){
-		for(int mapx = 0; mapx < map_sizeX; mapx++){
-				switch(map[mapy][mapx])
-				{
-					case TILE_FLOOR:
-						mvaddch(mapy, mapx, '.');
-						break;
-					case TILE_WALL:
-						mvaddch(mapy, mapx, '#');
-						break;
-				}
+void drawTile(){
+	for(int mapy = 0; mapy < MAP_HEIGHT; mapy++){
+		for(int mapx = 0; mapx < MAP_WIDTH; mapx++){
+			// get tile type
+			int nType = map[mapy][mapx];
+
+			attron(COLOR_PAIR(sTileIndex[nType].nColorCode));
+			mvaddch(mapy, mapx, sTileIndex[nType].nCharacter);
+			attroff(COLOR_PAIR(sTileIndex[nType].nColorCode));
 		}
 	}
 }
@@ -131,15 +156,71 @@ void drawMap(){
 // function: check if a tile is passable
 bool isPassable(int nMapX, int nMapY){
 	// check if coordinates are valid; i.e. can't walk off the map
-	if(nMapX < 0 || nMapX >= map_sizeX || nMapY < 0 || nMapY >= map_sizeY)
+	if(nMapX < 0 || nMapX >= MAP_WIDTH || nMapY < 0 || nMapY >= MAP_HEIGHT)
 		return false;
 
-	// grab tile information from map
-	int tileValue = map[nMapY][nMapX];
+	// grab tile information from map array
+	int nTileValue = map[nMapY][nMapX];
+	return sTileIndex[nTileValue].bPassable;
+}
 
-	// return true if tile is passable; otherwise return false
-	if(tileValue == TILE_FLOOR)
-		return true;
+void interactDoor(){
+	mvprintw(MAP_HEIGHT + 1, 0, "What direction?");
+	int ch = getch();
+	int nDeltaX, nDeltaY;
+	int nTileValue;
 
-	return false;
+	switch(ch){
+	// upper-left
+		case 'y':
+			nDeltaX = -1;
+			nDeltaY = -1;
+			break;
+		// upper-right
+		case 'u':
+			nDeltaX = 1;
+			nDeltaY = -1;
+			break;
+		// left
+		case 'h':
+			nDeltaX = -1;
+			nDeltaY = 0;
+			break;
+		// down
+		case 'j':
+			nDeltaX = 0;
+			nDeltaY = 1;
+			break;
+		// up
+		case 'k':
+			nDeltaX = 0;
+			nDeltaY = -1;
+			break;
+		// right
+		case 'l':
+			nDeltaX = 1;
+			nDeltaY = 0;
+			break;
+		// bottom-left
+		case 'b':
+			nDeltaX = -1;
+			nDeltaY = 1;
+			break;
+		// bottom-right
+		case 'n':
+			nDeltaX = 1;
+			nDeltaY = 1;
+			break;
+		default:
+			clear();
+			return;
+	}
+
+	if(map[nPlayerY + nDeltaY][nPlayerX + nDeltaX] == TILE_CLOSEDDOOR){
+		map[nPlayerY + nDeltaY][nPlayerX + nDeltaX] = TILE_OPENDOOR;
+	}
+	else if(map[nPlayerY + nDeltaY][nPlayerX + nDeltaX] == TILE_OPENDOOR)
+	{
+		map[nPlayerY + nDeltaY][nPlayerX + nDeltaX] = TILE_CLOSEDDOOR;
+	}
 }
